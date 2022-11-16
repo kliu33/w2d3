66b0a0ec705365d696352e329e1eec35f656f276
kliu33 <kliu33@buffalo.edu>
2022-11-16 16:53:18 -0500

    board can check if there is a winner

--- tictactoe/board.rb
+++ tictactoe/board.rb
@@ -1,4 +1,7 @@
 class Board
+
+    attr_reader :grid
+
     def initialize(n)
         @grid = Array.new(n) {Array.new(n, "_")}
     end
@@ -29,10 +32,59 @@ class Board
 
     def place_mark(pos, val)
         if valid?(pos) && empty?(pos)
-            self[pos] = val 
+            self[pos] = val.to_s
         else
             raise RuntimeError
         end
+    end
+
+    def printy
+        @grid.each_with_index do |subarr, idx|
+            subarr.each_with_index do |ele, idx2|
+                print "#{ele} "
+            end
+            print "\n"
+        end
+    end
+
+    def win?(mark)
+        (0...@grid.length).each do |y|
+            same = 0
+            (0...@grid.length).each do |x|
+                if mark == @grid[x][y]
+                    same += 1
+                end
+            end
+            return true if same == @grid.length
+        end
+        (0...@grid.length).each do |y|
+            same = 0
+            (0...@grid.length).each do |x|
+                if mark == @grid[y][x]
+                    same += 1
+                end
+            end
+            return true if same == @grid.length
+        end
+        same = 0
+        (0...@grid.length).each do |num|
+            if @grid[num][num] == mark
+                same += 1
+            end
+            return true if same == @grid.length
+        end
+        same = 0
+        (0...@grid.length).each do |num|
+            if @grid[@grid.length - 1 - num][num] == mark
+                same += 1
+            end
+            return true if same == @grid.length
+        end
+        false
+    end
+
+    def empty_positions?
+        @grid.flatten.count("_") > 0
     end
 
 end 
