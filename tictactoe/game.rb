c5c8df4d2bd10eb2773d2f3d7336ee860aa1a6c2
kliu33 <kliu33@buffalo.edu>
2022-11-16 17:49:17 -0500

    multiple players update

--- tictactoe/game.rb
+++ tictactoe/game.rb
@@ -2,24 +2,24 @@ require_relative "board"
 require_relative "human_player"
 
 class Game
-    def initialize(mark1,mark2)
-        @mark1 = mark1
-        @mark2 = mark2
-        @p1= HumanPlayer.new(mark1)
-        @p2 = HumanPlayer.new(mark2)
-        @board = Board.new(3)
-        @current_player = @p1
+    def initialize(size,*marks)
+        @marks = marks
+        @players = []
+        @marks.each do |mark|
+            @players << HumanPlayer.new(mark)
+        end
+        @board = Board.new(size)
     end
 
     def switch_turn
-        @current_player == @p1 ? @current_player = @p2 : @current_player = @p1
+        @players.rotate!
     end
 
     def play
         while !@board.empty_positions? 
             @board.printy
-            @board.place_mark(@current_player.get_position, @current_player.mark)
-            if @board.win?(@current_player.mark)
+            @board.place_mark(@players[0].get_position, @players[0].mark)
+            if @board.win?(@players[0].mark)
                 print "VICTORY!!"
                 return
             end
