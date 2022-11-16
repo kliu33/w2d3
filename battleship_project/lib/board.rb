bd1d24e3d29aac199c852ea26439159ce6529061
kliu33 <kliu33@buffalo.edu>
2022-11-16 14:56:13 -0500

    added place random ships method

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,6 +1,6 @@
 class Board
 
-  attr_reader :size
+  attr_reader :size, :grid
 
   def initialize(n)
     @grid = Array.new(n) {Array.new(n, :N)}
@@ -19,4 +19,27 @@ class Board
     @grid.flatten.count(:S)
   end
 
+  def attack(pos)
+    if self[pos] == :S
+        self[pos] = :H
+        p "you sunk my battleship"
+        return true
+    else
+        self[pos] = :X
+        return false
+    end
+  end
+
+  def place_random_ships
+    place = size / 4
+    while num_ships != place
+        x = rand(0...self.grid.length)
+        y = rand(0...self.grid.length)
+        if self[[x,y]] == :N
+            self[[x,y]] = :S
+        end
+    end
+  end
+
+  
 end
